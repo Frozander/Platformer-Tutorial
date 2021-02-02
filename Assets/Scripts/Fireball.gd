70ebaebd2a4e189a23ab23a3fc079be9e81547bd
Mustafa Batuhan Ceylan <ceylan.mustafabatuhan@gmail.com>
2021-02-02 21:07:56 +0300

    Added various checks for animations

--- Assets/Scripts/Fireball.gd
+++ Assets/Scripts/Fireball.gd
@@ -3,14 +3,23 @@ extends Area2D
 const V = 100;
 
 var motion = Vector2()
+var direction = 1
 
 func _ready():
 	$Sprite.play("fire")
 
 func _physics_process(delta):
-	motion.x = V * delta
+	motion.x = V * delta * direction
 	translate(motion)
 	
 
 func _on_VisibilityNotifier2D_viewport_exited(_viewport):
 	queue_free()
+	
+func _on_Fireball_body_entered(body):
+	queue_free()
+
+func set_fireball_dir(dir):
+	direction = dir
+	if direction == -1:
+		$Sprite.flip_h = true
--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -7,33 +7,57 @@ const J = -250
 
 const FIREBALL = preload("res://Assets/Prefabs/Fireball.tscn")
 
+var is_attacking = false
+var is_on_ground = true
 var motion = Vector2()
 
 func _physics_process(_delta):
+	is_on_ground = is_on_floor()
 	motion.y += G
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = V
-		$Sprite.flip_h = false
-		$Sprite.play("run")
+		if !is_attacking || !is_on_ground:
+			motion.x = V
+			if !is_attacking:
+				$Sprite.flip_h = false
+				$Sprite.play("run")
+				if sign($FireballLocation.position.x) == -1:
+					$FireballLocation.position.x *= -1
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -V
-		$Sprite.flip_h = true
-		$Sprite.play("run")
+		if !is_attacking || !is_on_ground:
+			motion.x = -V
+			if !is_attacking:
+				$Sprite.flip_h = true
+				$Sprite.play("run")
+				if sign($FireballLocation.position.x) == 1:
+					$FireballLocation.position.x *= -1
 	else:
 		motion.x = 0
-		$Sprite.play("idle")
+		if !is_attacking && is_on_ground:
+			$Sprite.play("idle")
 		
-	if is_on_floor():
+	if is_on_ground && !is_attacking:
 		if Input.is_action_pressed("ui_up"):
 			motion.y += J
-	else:
-		$Sprite.play("jump")
+	if !is_on_ground && !is_attacking:
+		if motion.y < 0:
+			$Sprite.play("jump")
+		else:
+			$Sprite.play("fall")
 	
-	if Input.is_action_just_pressed("ui_attack"):
+	if Input.is_action_just_pressed("ui_attack") && !is_attacking:
+		if is_on_ground:
+			motion.x = 0
+		is_attacking = true
+		$Sprite.play("slash")
 		var fireball_instance = FIREBALL.instance()
 		fireball_instance.position = $FireballLocation.global_position
+		# While having if/else statement would look more readable
+		# I prefer "possibly" faster logic to readable logic (Also tihs doesn't look that bad)
+		fireball_instance.set_fireball_dir(sign($FireballLocation.position.x))
 		get_parent().add_child(fireball_instance)
-		$Sprite.play("slash")
 	
 	motion = move_and_slide(motion, UP)
+
+func _on_Sprite_animation_finished():
+	is_attacking = false
