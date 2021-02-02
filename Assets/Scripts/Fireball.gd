5e33ca257f812d7d266c140a5e8f2e92796adf3e
Mustafa Batuhan Ceylan <ceylan.mustafabatuhan@gmail.com>
2021-02-02 21:09:32 +0300

    changed 'body' variable in '_on_Fireball_body_entered(body)' to '_body' for code sytle reasons

--- Assets/Scripts/Fireball.gd
+++ Assets/Scripts/Fireball.gd
@@ -16,7 +16,7 @@ func _physics_process(delta):
 func _on_VisibilityNotifier2D_viewport_exited(_viewport):
 	queue_free()
 	
-func _on_Fireball_body_entered(body):
+func _on_Fireball_body_entered(_body):
 	queue_free()
 
 func set_fireball_dir(dir):
